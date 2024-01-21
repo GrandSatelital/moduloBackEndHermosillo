840802ad681d942285e0e717cbc7564bf60a487a
gdHermosillo <gadiel.ddelgado@gmail.com>
2024-01-21 02:21:09 -0600

    solved exc3

--- ej1.js
+++ ej1.js
@@ -3,6 +3,10 @@ Crear un script que reciba [n] cantidad de argumentos y los muestre en la termin
 Nota: No debe mostrar la ruta de node ni del script
 */
 
-for (let i = 2; i < process.argv.length; i++) {
-  console.log(process.argv[i]);
-}
+const showArgs = () => {
+  for (let i = 2; i < process.argv.length; i++) {
+    console.log(process.argv[i]);
+  }
+};
+
+showArgs();
--- ej3.js
+++ ej3.js
@@ -1,6 +1,9 @@
 // Crear un script que reciba n cantidad de argumentos y que los muestre en terminal de manera inversa a la ingresada
 // Nota: no mostrar path node ni script
 
-for (let i = process.argv.length; i < process.argv.length; i--) {
-  console.log(process.argv[i]);
-}
+const showArgsInverseOrder = () => {
+  for (let i = process.argv.length - 1; i >= 2; i--) {
+    console.log(process.argv[i]);
+  }
+};
+showArgsInverseOrder();
