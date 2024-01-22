d6cbe1d0bbced1415ca752b84896c6906279a7b8
gdHermosillo <gadiel.ddelgado@gmail.com>
2024-01-22 01:43:47 -0600

    .

--- session2Ex/fs.js
+++ session2Ex/fs.js
@@ -1,1 +1,28 @@
 const fs = require("fs");
+
+/*fs.writeFile("./example.txt", "Hola Koders!", (err) => {
+  if (err) throw err;
+  console.log("File created succesfully");
+});
+*/
+
+/*fs.writeFile("./deleteFile.txt", "This will be deleted", (err) => {
+  if (err) throw err;
+  console.log("File created");
+});
+
+/*fs.readFile("./example.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+*/
+
+fs.appendFile("./example.txt", ` Updated ${new Date()}`, (err) => {
+  if (err) throw err;
+  console.log("File updated");
+});
+
+/*fs.unlink("./deleteFile.txt", (err) => {
+  if (err) throw err;
+  console.log("File deleted");
+});*/
