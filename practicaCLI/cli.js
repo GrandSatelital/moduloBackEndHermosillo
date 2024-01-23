dc6e77539f238cccabb3d862c22ca5e8bde6bb6d
gdHermosillo <gadiel.ddelgado@gmail.com>
2024-01-23 04:41:57 -0600

    .

--- practicaCLI/cli.js
+++ practicaCLI/cli.js
@@ -15,7 +15,7 @@ const startProject = () => {
 };
 
 const createHtml = () => {
-  fs.writeFile(fileName + ".html", "", (err) => {
+  fs.writeFile("./" + fileName + ".html", "", (err) => {
     if (err) throw err;
     console.log("File created");
   });
@@ -61,4 +61,74 @@ const createCss = () => {
   });
 };
 
-createCss();
+switch (command) {
+  case "init":
+    const startProject = () => {
+      fs.mkdir(fileName, (err) => {
+        if (err) throw err;
+        console.log("Folder created");
+        fs.writeFile(fileName + "/index.html", "", (err) => {
+          if (err) throw err;
+          console.log("File created");
+        });
+      });
+    };
+    startProject();
+    break;
+
+  case "html":
+    const createHtml = () => {
+      fs.writeFile(fileName + ".html", "", (err) => {
+        if (err) throw err;
+        console.log("File created");
+      });
+    };
+    createHtml();
+    break;
+
+  case "js":
+    const createJs = () => {
+      fs.access("./js", fs.constants.F_OK, (err) => {
+        if (err) {
+          fs.mkdir("./js", (err) => {
+            if (err) throw err;
+            console.log("Folder js created");
+            fs.writeFile("./js/" + fileName + ".js", "", (err) => {
+              if (err) throw err;
+              console.log(`File ${fileName}.js created`);
+            });
+          });
+        } else {
+          fs.writeFile("./js/" + fileName + ".js", "", (err) => {
+            if (err) throw err;
+            console.log(`File ${fileName}.js created`);
+          });
+        }
+      });
+    };
+    createJs();
+    break;
+
+  case "css":
+    const createCss = () => {
+      fs.access("./css", fs.constants.F_OK, (err) => {
+        if (err) {
+          fs.mkdir("./css", (err) => {
+            if (err) throw err;
+            console.log("Folder css created");
+            fs.writeFile("./css/" + fileName + ".css", "", (err) => {
+              if (err) throw err;
+              console.log(`File ${fileName}.css created`);
+            });
+          });
+        } else {
+          fs.writeFile("./css/" + fileName + ".css", "", (err) => {
+            if (err) throw err;
+            console.log(`File ${fileName}.css created`);
+          });
+        }
+      });
+    };
+    createCss();
+    break;
+}
