317713b4c443e9cbf08b698852a27d7efdbe156e
gdHermosillo <gadiel.ddelgado@gmail.com>
2024-01-23 04:29:43 -0600

    .

--- practicaCLI/cli.js
+++ practicaCLI/cli.js
@@ -20,3 +20,45 @@ const createHtml = () => {
     console.log("File created");
   });
 };
+
+const createJs = () => {
+  fs.access("./js", fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.mkdir("./js", (err) => {
+        if (err) throw err;
+        console.log("Folder js created");
+        fs.writeFile("./js/" + fileName + ".js", "", (err) => {
+          if (err) throw err;
+          console.log(`File ${fileName}.js created`);
+        });
+      });
+    } else {
+      fs.writeFile("./js/" + fileName + ".js", "", (err) => {
+        if (err) throw err;
+        console.log(`File ${fileName}.js created`);
+      });
+    }
+  });
+};
+
+const createCss = () => {
+  fs.access("./css", fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.mkdir("./css", (err) => {
+        if (err) throw err;
+        console.log("Folder css created");
+        fs.writeFile("./css/" + fileName + ".css", "", (err) => {
+          if (err) throw err;
+          console.log(`File ${fileName}.css created`);
+        });
+      });
+    } else {
+      fs.writeFile("./css/" + fileName + ".css", "", (err) => {
+        if (err) throw err;
+        console.log(`File ${fileName}.css created`);
+      });
+    }
+  });
+};
+
+createCss();
