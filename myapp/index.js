2d2499dfeaa714948c5ddb6240fecd9aea0043da
gdHermosillo <gadiel.ddelgado@gmail.com>
2024-01-24 21:55:52 -0600

    .

--- myapp/index.js
+++ myapp/index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const hostname = "localhost";
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Koders");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/hi", (req, res) => {
+  res.send("Hi!");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Users page");
+});
+
+app.post("/", (req, res) => {
+  res.status(201).send("Post aceptado");
+});
+
+app.post("/users", (req, res) => {
+  res.status(201).send("User accepted");
+});
+
+app.put("/users", (req, res) => {
+  res.status(200).send("User updated");
+});
+
+app.delete("/users", (req, res) => {
+  res.status(204).send("User deleted");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Example app listening on http://${hostname} port ${port}`);
 });
